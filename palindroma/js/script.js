080055eac8b1d78e3432ac8e208704655befdaff
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-03-25 15:54:40 +0100

    primo esercizio

---
+++ palindroma/js/script.js
@@ -0,0 +1,33 @@
+// l'utente inserisce una parola 
+var parolaUtente = prompt('Dimmi una parola');
+console.log(parolaUtente);
+
+
+function confrontoPalindroma (parolaUno, parolaDue) {
+//creare due for uno da sinistra e uno da destra
+//
+//legge da sinistra a destra
+for( var i = 0; i < parolaUtente.length; i++ )
+var parolaUno = parolaUtente[i];
+console.log(parolaUno);
+
+//legge da destra a sinistra
+for( var j = parolaUtente.length - 1; j >= 0; j-- )
+var parolaDue = parolaUtente[j];
+console.log(parolaDue);
+var conclusione;
+
+//confrontare i due for
+if( parolaUno == parolaDue ){
+    conclusione = ('la parola è palindroma')
+} else {
+    conclusione = ('la parola non è polindroma')
+}
+
+// stampa il risultato del confronto tra le due parole
+alert(conclusione);
+}
+
+var messaggioFinale = confrontoPalindroma(parolaUtente, parolaUtente)
+
+//stampare il messaggio
